247af3b34c2c2ed9b2b7e680d8f2b8f26d2c6c55
Swapnil Bhavsar <hey@swapnil.dev>
2023-05-07 12:01:07 +0530

    Fix issue with password hash

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,10 +14,14 @@ class RegisterController extends Controller
         $data = $request->validate([
             'name'  => ['required'],
             'email' => ['required', 'email', 'unique:users'],
-            'password' => ['required', 'confirmed']
+            'password' => ['required']
         ]);
 
-        $user = User::create($data);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
 
         $token = $user->createToken('login-token');
 
