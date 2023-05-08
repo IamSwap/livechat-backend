6647db13b93423d1353a6a770f057692c1289c6a
Swapnil Bhavsar <hey@swapnil.dev>
2023-05-08 17:02:31 +0530

    Use native pusher config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,11 +37,11 @@ return [
             'app_id' => env('PUSHER_APP_ID', 'app-id'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_PORT', 6001),
-                'scheme' => env('PUSHER_SCHEME', 'http'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME' ) === 'https',
+                // 'host' => env('PUSHER_HOST', '127.0.0.1'),
+                // 'port' => env('PUSHER_PORT', 6001),
+                // 'scheme' => env('PUSHER_SCHEME', 'http'),
+                // 'encrypted' => true,
+                // 'useTLS' => env('PUSHER_SCHEME' ) === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
